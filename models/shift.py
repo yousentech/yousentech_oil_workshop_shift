63d340c2676ac57d0f5c5f171beb105d138b73af
yousentech <83219678+yousentech@users.noreply.github.com>
2025-10-28 23:43:12 +0300

    update

--- models/shift.py
+++ models/shift.py
@@ -20,6 +20,8 @@ class OilShift(models.Model):
     expected_cash = fields.Float(string='Expected Cash', digits='Product Price', compute='_compute_expected', store=True)
     difference = fields.Float(string='Cash Difference', digits='Product Price', compute='_compute_difference', store=True)
     sales_difference = fields.Float(string='Sales Difference', digits='Product Price', compute='_compute_difference', store=True)
+    orders_num = fields.Float(string='Orders Num', digits='Product Price', compute='_compute_difference', store=True)
+    orders_invoiced_num = fields.Float(string='Orders Invoiced Num', digits='Product Price', compute='_compute_difference', store=True)
     note = fields.Text(string='Notes')
     state = fields.Selection([('open','Open'),('closed','Closed')], default='open', string='Status', tracking=True)
 
@@ -93,7 +95,8 @@ class OilShift(models.Model):
         for rec in self:
             rec.difference = (rec.cash_end or 0.0) - (rec.expected_cash or 0.0)
             rec.sales_difference = ((rec.sale_total or 0.0) ) - ((rec.sale_cash_total or 0.0) + (rec.sale_card_total or 0.0) )
-    
+            rec.orders_invoiced_num = len(rec.sale_ids.account_move_id.ids)
+            rec.orders_num = len(rec.sale_ids.ids)
     def action_print_thermal(self):
         """طباعة تقرير الشفت بالطابعة الحرارية"""
         self.ensure_one()

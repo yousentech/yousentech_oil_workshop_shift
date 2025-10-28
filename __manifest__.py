2ead028befcf2aba8fd4019b958a8e182f6f6437
yousentech <83219678+yousentech@users.noreply.github.com>
2025-10-28 18:52:24 +0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
         'views/menus.xml',
         'views/shift_views.xml',
         'views/expense_views.xml',
-        # 'views/sale_views.xml',
+        'views/sale_views.xml',
         # 'reports/shift_report_thermal.xml',
         # 'reports/shift_report_formal.xml',
         # 'reports/shift_report_actions.xml',

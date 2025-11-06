7f2e4f2a990774830c7d25b36133e87c8bc1db51
yousentech <83219678+yousentech@users.noreply.github.com>
2025-11-06 11:37:55 +0300

    upfsat

--- models/shift.py
+++ models/shift.py
@@ -46,7 +46,7 @@ class OilShift(models.Model):
     # def action_open_shift(self):
     #     for rec in self:
     #         existing = self.search([('user_id','=',rec.user_id.id),('state','=','open'),('id','!=',rec.id)])
-    #         if existing:
+        #         if existing:
     #             raise UserError(_('Cannot open this shift because another open shift exists (%s).') % existing[0].name)
     #         rec.state = 'open'
     #         rec.start_time = fields.Datetime.now()

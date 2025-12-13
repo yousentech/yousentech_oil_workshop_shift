7513e55208c63f613d0eb722292baaffd97fd121
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-13 14:20:41 +0300

    updte

--- models/sale_extension.py
+++ models/sale_extension.py
@@ -6,12 +6,41 @@ class OilWorkOrder(models.Model):
 
     shift_id = fields.Many2one('oil.shift', string='Shift',)
 
+    shift_limit_type = fields.Char(
+        default=lambda self: self._default_shift_limit_type(),
+        compute="_check_shift_limit_type",
+    )
+    def _default_shift_limit_type(self):
+
+        params = self.env['ir.config_parameter'].sudo()
+        shift_limit_flag = params.get_param('shift_limit',
+                                             default=False)
+        
+        return shift_limit_flag
+
+
+ 
+
+    def _check_shift_limit_type(self):
+
+        params = self.env['ir.config_parameter'].sudo()
+        shift_limit_flag = params.get_param('shift_limit',
+                                            default=False)
+        for rec in self:
+            rec.shift_limit_type = shift_limit_flag
+        
     def _validate_entries(self):
 
         res = super()._validate_entries()
         if self.shift_id.start_time:
             if self.order_date.date() < self.shift_id.start_time.date():
                 raise UserError(_("THe order date is less than from shift start date - تاريخ امر العمل اقل من تاريخ بدء الشفت"))
+           
+            if self.shift_limit_type == 'daily':
+                if self.order_date.date() != open_shift.start_time.date():
+                    raise UserError('Warning .. The work order date does not match the open shift date.')
+                
+                 
 
         return res
 
@@ -36,37 +65,17 @@ class OilWorkOrder(models.Model):
             user_id = vals.get('user_id') or self.env.uid
             open_shift = self.env['oil.shift'].search([('company_id','=', vals.get('company_id')),('state','=','open')], limit=1)
             if open_shift:
-                vals['shift_id'] = open_shift.id
+                if self.shift_limit_type == 'daily':
+                    if self.order_date.date() == open_shift.start_time.date():
+                        vals['shift_id'] = open_shift.id
+                    else:
+                        raise UserError('You must Close opened shift and open new shift before recording a sale.')
+                else:
+                    vals['shift_id'] = open_shift.id
         
+                 
             if not open_shift:
                 raise UserError('You must open a shift before recording a sale.')
 
         return super().create(vals)
     
-
-    shift_limit_type = fields.Char(
-        default=lambda self: self._default_shift_limit_type(),
-        compute="_check_shift_limit_type",
-    )
-    def _default_shift_limit_type(self):
-
-        params = self.env['ir.config_parameter'].sudo()
-        shift_limit_flag = params.get_param('shift_limit',
-                                             default=False)
-        print("shift_limit_flag+++++++++++++++++++++++",shift_limit_flag)
-        return shift_limit_flag
-
-
- 
-
-    def _check_shift_limit_type(self):
-
-        params = self.env['ir.config_parameter'].sudo()
-        shift_limit_flag = params.get_param('shift_limit',
-                                            default=False)
-        for rec in self:
-            rec.shift_limit_type = shift_limit_flag
-        print("shift_limit_flag+++++++++++++++++++++++",shift_limit_flag)
-
-
- 

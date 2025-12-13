62e8f7ed63f6adbac71c8209d3b2bc0045939313
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-13 18:36:07 +0300

    update

--- models/sale_extension.py
+++ models/sale_extension.py
@@ -38,7 +38,7 @@ class OilWorkOrder(models.Model):
            
             if self.shift_limit_type == 'daily':
 
-                if  fields.Date.today() != self.shift_id.start_time.date():
+                if  fields.Date.today().date() != self.shift_id.start_time.date():
                     raise UserError('You must Close opened shift and open new shift before recording a sale.')
 
                 if self.order_date.date() != self.shift_id.start_time.date():

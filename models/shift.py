4535107d3e4a3c975eaa795b52821018086aa7ab
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-14 12:01:57 +0300

    updatr

--- models/shift.py
+++ models/shift.py
@@ -52,10 +52,10 @@ class OilShift(models.Model):
         return super().create(vals)
 
 
-    @api.depends('start_time')
-    def set_start_time_date(self)
-        for rec in self:
-            rec.start_time_date = rec.start_time
+    # @api.depends('start_time')
+    # def set_start_time_date(self):
+    #     for rec in self:
+    #         rec.start_time_date = rec.start_time
 
     # def action_open_shift(self):
     #     for rec in self:

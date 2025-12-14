0c6ce0064d6302daf5c8aaf86d2eebc6ac55efb4
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-14 15:34:42 +0300

    update

--- models/sale_extension.py
+++ models/sale_extension.py
@@ -69,11 +69,11 @@ class OilWorkOrder(models.Model):
             user_id = vals.get('user_id') or self.env.uid
             open_shift = self.env['oil.shift'].search([('company_id','=', vals.get('company_id')),('state','=','open')], limit=1)
             if open_shift:
-                print('self.created_order_date',self.created_order_date)
+                print('self.created_order_date',vals.get('created_order_date'))
                 print('open_shift.start_time_date',open_shift.start_time_date)
                 print(' fields.Date.today()', fields.Date.today())
                 if self.shift_limit_type == 'daily':
-                    if self.created_order_date == open_shift.start_time_date and fields.Date.today() == open_shift.start_time_date:
+                    if vals.get('created_order_date') == open_shift.start_time_date and fields.Date.today() == open_shift.start_time_date:
                         vals['shift_id'] = open_shift.id
                     else:
                         raise UserError('You must Close opened shift and open new shift before recording a sale.')

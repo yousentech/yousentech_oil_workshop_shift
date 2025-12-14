c5839d66319de85626badaf426638ed0fd36738b
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-14 15:39:01 +0300

    update

--- models/sale_extension.py
+++ models/sale_extension.py
@@ -72,9 +72,9 @@ class OilWorkOrder(models.Model):
                 print('self.created_order_date',vals.get('created_order_date'))
                 print('open_shift.start_time_date',open_shift.start_time_date)
                 print(' fields.Date.today()', fields.Date.today())
-                print('self.shift_limit_type', self.shift_limit_type)
+                print('vals.get('shift_limit_type')', vals.get('shift_limit_type'))
                 
-                if self.shift_limit_type == 'daily':
+                if vals.get('shift_limit_type') == 'daily':
                     if vals.get('created_order_date') == open_shift.start_time_date and fields.Date.today() == open_shift.start_time_date:
                         vals['shift_id'] = open_shift.id
                     else:

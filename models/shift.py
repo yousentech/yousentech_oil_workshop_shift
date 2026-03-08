e72c9320a1cd1840bf45ac5a2e238acfe7f9f0a1
yousentech <83219678+yousentech@users.noreply.github.com>
2026-03-09 02:24:21 +0300

    update

--- models/shift.py
+++ models/shift.py
@@ -53,6 +53,7 @@ class OilShift(models.Model):
     @api.depends("company_id")
     def _compute_allow_change_shfit_num(self):
         for rec in self:
+            
             rec.allow_change_shfit_num = self.user_has_groups("yousentech_oil_workshop_shift.group_allow_modify_shift_in_wo")
 
     @api.model
